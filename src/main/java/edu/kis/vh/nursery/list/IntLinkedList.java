23b72523b5ed3bb6f2f53309c927c890e7306482
LeskiewiczMichal <leskiewicz02@gmail.com>
2025-02-03 21:15:28 +0100

    5. zmiana nazw pol w klasach

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -10,7 +10,7 @@ public class IntLinkedList {
             last = new Node(i);
         else {
             last.next = new Node(i);
-            last.next.prev = last;
+            last.next.previous = last;
             last = last.next;
         }
     }
@@ -33,7 +33,7 @@ public class IntLinkedList {
         if (isEmpty())
             return -1;
         int ret = last.value;
-        last = last.prev;
+        last = last.previous;
         return ret;
     }
 
